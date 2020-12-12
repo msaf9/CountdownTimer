4efd637cdfcb661ee244bebe5b5555e6ee979e5e
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2020-12-12 13:16:07 +0530

    Timer created - First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Counter';
+  constructor() {
+
+    var countDownDate = new Date("Dec 12, 2020 14:00:00").getTime();
+
+    var x = setInterval(function () {
+
+      var now = new Date().getTime();
+      var duration = countDownDate - now;
+      var days = Math.floor(duration / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((duration % (1000 * 60)) / 1000);
+      document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+      if (duration < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+  }
+
+
 }
+
